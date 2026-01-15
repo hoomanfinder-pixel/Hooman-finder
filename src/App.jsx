4a8c446cda272f12de1f05db0360e04bf71396a2
Lauren Breukink <hoomanfinder@gmail.com>
2026-01-15 16:41:19 -0500

    chore: add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import Home from "./pages/Home.jsx";
 import Quiz from "./pages/Quiz.jsx";
@@ -10,22 +11,27 @@ import JoinShelters from "./pages/JoinShelters.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/quiz" element={<Quiz />} />
-      <Route path="/results" element={<Results />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<Results />} />
 
-      {/* Dog profile page */}
-      <Route path="/dog/:id" element={<DogDetail />} />
+        {/* Dog profile page */}
+        <Route path="/dog/:id" element={<DogDetail />} />
 
-      {/* Shelter profile page */}
-      <Route path="/shelter/:id" element={<Shelter />} />
+        {/* Shelter profile page */}
+        <Route path="/shelter/:id" element={<Shelter />} />
 
-      {/* ✅ Shelter onboarding / join page */}
-      <Route path="/shelters/join" element={<JoinShelters />} />
+        {/* Shelter onboarding / join page */}
+        <Route path="/shelters/join" element={<JoinShelters />} />
 
-      {/* Fallback */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
+      {/* Vercel Analytics */}
+      <Analytics />
+    </>
   );
 }
